f85d133bfb51f98f0b68bdae8c5c07572a13bcb2
GemechisUrgessa <gemechisurgessa@gmail.com>
2023-02-21 21:39:48 +0300

    main updated

--- main.go
+++ main.go
@@ -16,6 +16,13 @@ import (
 // It connects to peers and downloads the file
 // It then starts seeding the file to the peers that are connected to it and waits for the user to press enter to exit
 func main() {
+	// Check if the correct number of arguments are passed in
+	if len(os.Args) != 3 {
+		fmt.Println("Usage: go run main.go <path to .torrent file> <path to file to download to>")
+		return
+	}
+
+	// Get the paths to the .torrent file and the file to download to from the arguments
 	inPath := os.Args[1]
 	outPath := os.Args[2]
 
@@ -31,12 +38,14 @@ func main() {
 	}
 
 	// Connect to peers and download file and start seeding
+	fmt.Println("Connecting to peers...")
 	keepAliveChan := make(chan bool)
 	clients, err := torrent.ConnectToPeers(tor, keepAliveChan)
 	fmt.Printf("Number of clients is %d\n", len(clients))
 	if err != nil {
 		log.Fatal(err)
 	}
+	// Start a goroutine to send keep alive messages to the peers
 	go func() {
 		for {
 			select {
@@ -49,6 +58,7 @@ func main() {
 	}()
 
 	// Download file and start seeding
+	fmt.Println("Downloading file....")
 	err = tf.DownloadToFile(outPath, tor, clients)
 	if err != nil {
 		log.Fatal(err)
@@ -58,12 +68,15 @@ func main() {
 	// Add one to the wait group
 	wg.Add(1)
 	// Start seeding the file
+
 	go func() {
 		defer wg.Done()
+		fmt.Println("Starting to seed file...")
 		seeder.SeedFile(clients, tor, outPath)
 	}()
 	// Wait for user to press enter to exit
 	fmt.Println("Leeching and seeding complete. Press enter to exit")
 	wg.Wait()
 	fmt.Println("Exiting...")
+
 }
