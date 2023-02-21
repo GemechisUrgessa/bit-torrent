86f1790374912da8dd8d27d71f6336f491160c39
ruhamags <91523337+ruhamags@users.noreply.github.com>
2023-02-21 18:09:55 +0300

    minor changes

--- bitfield/bitfield.go
+++ bitfield/bitfield.go
@@ -14,24 +14,3 @@ func (bf Bitfield) Len() int {
 	return len(bf) * 8
 }
 
-// HasPiece returns true if the bitfield has the given piece.
-func (bf Bitfield) HasPiece(index int) bool {
-	byteIndex := index / 8
-	offset := index % 8
-	if byteIndex < 0 || byteIndex >= len(bf) {
-		return false
-	}
-	return bf[byteIndex]>>(7-offset)&1 != 0
-}
-
-// SetPiece sets the given piece in the bitfield.
-func (bf Bitfield) SetPiece(index int) {
-	byteIndex := index / 8
-	offset := index % 8
-
-	// silently discard invalid bounded index.
-	if byteIndex < 0 || byteIndex >= len(bf) {
-		return
-	}
-	bf[byteIndex] |= 1 << (7 - offset)
-}
